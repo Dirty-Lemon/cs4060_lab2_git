01190742372813f6827640184b804ecc7208d6b8
DESKTOP-C7ODU2E\KevinTerceros <tercerosofficial23@gmail.com>
2026-02-28 16:14:45 -0700

    Replaced lines in task 4 with a simple math equation; finished assignment.

--- imgProcessingClass.py
+++ imgProcessingClass.py
@@ -5,14 +5,9 @@ import numpy as np
 
 class imgProcessing():
     
-    # Convert image to a cv2 readable image
-    def readcv(self, fileName):
-        cv2Img = cv2.imread(fileName, -1)
-        return cv2Img
-    
     # Load image in RGB
     def img2Rgb(self, fileName):
-        cv2Img = self.readcv(fileName)
+        cv2Img = cv2.imread(fileName, -1)
         imgRgb = cv2.cvtColor(cv2Img, cv2.COLOR_BGR2RGB)
         
         return imgRgb
@@ -26,13 +21,13 @@ class imgProcessing():
     # Get image with alpha mask
     def getAlphaMask(self, fileName):
         print("Applying alpha mask to", fileName, "...")
-        cv2Img = self.readcv(fileName)
+        cv2Img = cv2.imread(fileName, -1)
         
-        # print("Image Dimension ={}".format(cv2Img.shape))
         imgBGR = cv2Img[:, :, 0:3]
-        imgMask = cv2Img[:, :, 3]
+        imgMask = cv2Img[:, :, 3:4]
         
-        plt.figure(figsize = [15, 15])
+        # Show RGB and alpha mask versions of image
+        plt.figure(figsize = [10, 5])
         plt.subplot(121); plt.imshow(imgBGR[:, :, ::-1]); plt.title(fileName + ' (Colour Channels)')
         plt.subplot(122); plt.imshow(imgMask, cmap = 'gray'); plt.title(fileName + ' (Alpha Channels)')
         
@@ -47,35 +42,12 @@ class imgProcessing():
     # Properly overlay foreground image on to background using an alpha mask
     def applyMask(self, fgImg, bgImg, mask):
         
-        # # Extract the RGB channels from original image
-        # fgFloat = fgImg[:, :, 0]
-        # G_orig = origImg[:, :, 1]
-        # B_orig = origImg[:, :, 2]
-        
-        # # Extract the RGB channels from mask image
-        # R_mask = maskImg[0]
-        # G_mask = maskImg[1]
-        # B_mask = maskImg[2]
-        
-        # # Add alpha mask image channels to the original image channels
-        # newImg = (R_orig + alpha) + (G_orig + alpha) + (B_orig + alpha)
-        
-        # Extract RGB values from images to apply mask
-        # fgFloat = fgImg[:, :, :]
-        # bgFloat = bgImg[:, :, :]
-        # alpha = mask[:, :]
-        # fgImg = cv2.imread("panther.jpg")
-        # bgImg = cv2.imread("savana.jpg")
-        
+        # Convert images to data type usable in an equation
         fgFloat = fgImg.astype(float)
         bgFloat = bgImg.astype(float)
         alpha = mask.astype(float)/255
         
-        # Apply masking formula to over lay foreground on background
-        # newImg = alpha * fgFloat + (1 - alpha) * bgFloat
-        fgVal = cv2.multiply(alpha, fgFloat)
-        bgVal = cv2.multiply((1 - alpha), bgFloat)
-        newImg = cv2.add(fgVal, bgVal)
-        cv2.imshow("outImg", newImg/255)
+        # Apply masking formula to overlay foreground on to background
+        newImg = alpha * fgFloat + (1 - alpha) * bgFloat
         
-        return newImg
+        return newImg/255
--- mainClass.py
+++ mainClass.py
@@ -1,6 +1,4 @@
-import cv2
-import matplotlib.pyplot as plt
-from imgProcessingClass import imgProcessing
+from imgProcessingClass import *
 
 class main:
     i = imgProcessing()
@@ -17,14 +15,13 @@ class main:
     plt.show()
     
     # Task 3 - Resize image    
-    # image.shape = [height, width, channels]
+    # image.shape == [height, width, channels]
     bgImg_fitted = i.myResize(bgImg, pImg.shape[1], pImg.shape[0])
     i.showImg(bgName, bgImg_fitted)
     
     # Task 4 - Put panther on background image
-    # Logic for adding the values of the mask to the values of the background image and relayering the panther on the foreground
     maskedBg = i.applyMask(pImg, bgImg_fitted, pMask)
-    # i.showImg('Panther x Savana', maskedBg)
+    i.showImg('Panther x Savana', maskedBg)
     
 
 if __name__ == "__main__":
